44ec2e60af1c0a45be8ed5d0ded02f0408c06ece
nikolapivac <npivac00@fesb.hr>
2022-02-13 20:06:26 +0100

    Added products and started working on shop

---
+++ pages/shop.js
@@ -0,0 +1,12 @@
+import { Grid } from '@material-ui/core'
+import data from '../utils/data'
+
+export default function Shop(){
+    return(
+        <div>
+            {data.products.map((product, index) => (
+                
+            ))}
+        </div>
+    )
+}
---
+++ utils/data.js
@@ -0,0 +1,146 @@
+const data = {
+    products: [
+        {
+            name: "Dumbledore's wand",
+            category: "Wands",
+            image: "/images/shop/DumbledoreWand.webp",
+            price: "$40",
+            date: "November 3, 2018"
+        },
+        {
+            name: "Bellatrix Lastrange's wand",
+            category: "Wands",
+            image: "/images/shop/BellatrixWand.webp",
+            price: "$30",
+            date: "October 4, 2018"
+        },
+        {
+            name: "Cedric Diggory's wand",
+            category: "Wands",
+            image: "/images/shop/CedricWand.webp",
+            price: "$30",
+            date: "November 16, 2017"
+        },
+        {
+            name: "Dark Arts wand",
+            category: "Wands",
+            image: "/images/shop/DarkArtsWand.webp",
+            price: "$33",
+            date: "November 30, 2018"
+        },
+        {
+            name: "Draco Malfoy's wand",
+            category: "Wands",
+            image: "/images/shop/DracoWand.webp",
+            price: "$35",
+            date: "July 13, 2018"
+        },
+        {
+            name: "Chocolate Frog",
+            category: "Sweets",
+            image: "/images/shop/ChocoFrog.webp",
+            price: "$10",
+            date: "November 12, 2017"
+        },
+        {
+            name: "Ravenclaw Chocolate",
+            category: "Sweets",
+            image: "/images/shop/RChoco.webp",
+            price: "$5",
+            date: "November 30, 2018"
+        },
+        {
+            name: "Slytherin Dark Chocolate",
+            category: "Sweets",
+            image: "/images/shop/SChoco.webp",
+            price: "$5",
+            date: "November 3, 2018"
+        },
+        {
+            name: "Bootled Butter Beer",
+            category: "Sweets",
+            image: "/images/shop/Beer.webp",
+            price: "$8",
+            date: "June 30, 2018"
+        },
+        {
+            name: "Fudge Flies",
+            category: "Sweets",
+            image: "/images/shop/FudgeFlies.webp",
+            price: "$5",
+            date: "November 30, 2020"
+        },
+        {
+            name: "'R' Knitted Sweater",
+            category: "Clothes",
+            image: "/images/shop/RKnittedSweater.webp",
+            price: "$50",
+            date: "September 3, 2018"
+        },
+        {
+            name: "Gryffindor Cape",
+            category: "Clothes",
+            image: "/images/shop/GCape.webp",
+            price: "$45",
+            date: "March 6, 2018"
+        },
+        {
+            name: "Gryffindor Hoodie",
+            category: "Clothes",
+            image: "/images/shop/GHoodie.webp",
+            price: "$45",
+            date: "January 31, 2019"
+        },
+        {
+            name: "Slytherin Hat",
+            category: "Clothes",
+            image: "/images/shop/SHat.webp",
+            price: "$15",
+            date: "November 13, 2018"
+        },
+        {
+            name: "Hufflepuff Socks",
+            category: "Clothes",
+            image: "/images/shop/HSocks.webp",
+            price: "$20",
+            date: "August 5, 2019"
+        },
+        {
+            name: "Marauder's Map",
+            category: "Props",
+            image: "/images/shop/MaraudersMap.webp",
+            price: "$20",
+            date: "July 6, 2021"
+        },
+        {
+            name: "Ravenclaw Keychain",
+            category: "Props",
+            image: "/images/shop/RKeychain.webp",
+            price: "$7",
+            date: "July 6, 2020"
+        },
+        {
+            name: "The Fat Lady Pin",
+            category: "Props",
+            image: "/images/shop/FLPin.webp",
+            price: "$10",
+            date: "July 31, 2021"
+        },
+        {
+            name: "Gryffindor Backpack Set",
+            category: "Props",
+            image: "/images/shop/GBackpack.webp",
+            price: "$60",
+            date: "February 6, 2022"
+        },
+        {
+            name: "Slytherin Face Mask",
+            category: "Props",
+            image: "/images/shop/SMask.webp",
+            price: "$4",
+            date: "July 6, 2021"
+        },
+    ],
+};
+
+export default data;

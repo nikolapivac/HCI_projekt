853191617f199590244cea8f07e115472d3268b6
j-grgur-hci <josipa3371@gmail.com>
2022-02-13 19:49:21 +0100

    Added vertical line, image for google login, needs to be centered andto import font to tailwind

--- components/Experience.js
+++ components/Experience.js
@@ -4,11 +4,11 @@ export default function Experience({ experience }) {
   return (
     <>
     <div className='experiences'>
-      <div className='container fluid'>
-        <div className='row'>
-           <div className={styles.expcontainer} class="has-bg-img">
-    <img className={styles.exp_image} class="bg-img" src={experience.frontmatter.cover_image} alt='' />
+      <div className='container fluid hero-lg'>
+           <div className={styles.expcontainer}>
+    <img className={styles.exp_image} src={experience.frontmatter.cover_image} alt='' />
     </div>
+        <div className='row'>
           <div className='col'>
              <h3 className={styles.exp_title}>{experience.frontmatter.title}</h3>
     	          <div className={styles.text_container}>

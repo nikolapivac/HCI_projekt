ae3673c325db2f4fc447034bd0acf2e57cc34cc9
j-grgur-hci <josipa3371@gmail.com>
2022-02-15 23:13:36 +0100

    Mobile version for films page added

--- components/Film.js
+++ components/Film.js
@@ -7,17 +7,17 @@ export default function Film({ film }) {
     <div className={styles.filmscontainer}>
 <div className={styles.film}>
         <div className='row'>
-          <div className='col'>
+          <div className='col-md-7 '>
 <h3 className={styles.film_title}>{film.frontmatter.title}</h3>
 <p className={styles.film_body}>{film.frontmatter.excerpt}</p> 
           </div>
-          <div className='col-5'>
-            <img class="img fluid" className={styles.film_image} src={film.frontmatter.cover_image} alt='' />
+          <div className='col-md-5'>
+            <img  className={styles.film_image} src={film.frontmatter.cover_image} alt='' />
           </div>
 
         </div>
       </div>
-    </div>
+      </div>
     </>
   )
 }

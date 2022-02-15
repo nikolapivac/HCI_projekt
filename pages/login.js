a32a71f25d7308e1b5aa2339d665855993ea8406
nikolapivac <npivac00@fesb.hr>
2022-02-15 10:43:07 +0100

    Worked on CSS for login page

--- pages/login.js
+++ pages/login.js
@@ -13,47 +13,47 @@ export default function LoginPage() {
             <Head>
                 <title>Login</title>
             </Head>
-            <main className="flex flex-col items-center justify-center w-full flex-1 p-20 font-Wizarding">
-                <div className="bg-hp_blue text-text_yellow relative flex w-2/3">
-                    <div className="w-3/5 p-6">
-                        <h1 className="text-4xl mb-3 ml-4">Join Us</h1>
+            <main className={styles.main_container}>
+                <div className={styles.container}>
+                    <div className={styles.sign_up}>
+                        <h1 className={styles.joinus}>Join Us</h1>
 
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="name"><b>Name</b></label>
                             <input className ="mb-3" type="text" placeholder="" name="name" required></input>
                         </div>
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="uname"><b>Username</b></label>
                             <input className ="mb-3" type="text" placeholder="" name="uname" required></input>
                         </div>
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="psw"><b>Password</b></label>
                             <input className ="mb-3" type="password" placeholder="" name="psw" required></input>
                         </div>
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="dob"><b>Date of birth</b></label>
                             <input className ="mb-3" type="date" placeholder="" name="dob" required></input>
                         </div>
 
-                        <button className="bg-text_yellow text-hp_blue px-2 py-1 rounded-md ml-20 hover:text-hov_yellow">Sign Up</button>   
+                        <button className={styles.button}>Sign Up</button>   
                     </div>
-<h2 className={styles.verticalline}></h2>
-                    <div className="w-2/5 p-6">
-                        <h2 className="text-3xl mb-3 ml-4">Already have an account?</h2>
+<h2 className={styles.line}></h2>
+                    <div className={styles.login}>
+                        <h2 className={styles.already}>Already have an account?</h2>
 
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="uname"><b>Username</b></label>
                             <input className ="mb-3" type="text" placeholder="" name="uname" required></input>
                         </div>
-                        <div className="flex-col ml-7">
+                        <div className={styles.signup_form}>
                             <label className="flex" for="psw"><b>Password</b></label>
                             <input className ="" type="password" placeholder="" name="psw" required></input>
                         </div>
-                        <h4 className="text-xs mt-1 ml-20">Forgot your password?</h4>
+                        <h4 className={styles.forgot}>Forgot your password?</h4>
 
-                        <button className="bg-text_yellow text-hp_blue px-2 py-1 rounded-md ml-20 mt-1 hover:text-hov_yellow">Login</button>
+                        <button className={styles.button}>Login</button>
                         <div className={styles.gimg}>
-                        <Image src={gimg} class="px-2 py-1 rounded-md ml-20 mt-2"></Image>
+                        <Image src={gimg} class="px-2 py-1 rounded-md ml-20 mt-2 hover:cursor-pointer"></Image>
                         </div>
                     </div>
                 </div>
